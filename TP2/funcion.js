1f838f79a28f3625ec4313d301ce2ae55a23a0e2
Janusa <129460842+janusa13@users.noreply.github.com>
2023-04-27 21:23:47 -0300

    Add files via upload

---
+++ TP2/funcion.js
@@ -0,0 +1,10 @@
+function consulta() {
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+    
+}
