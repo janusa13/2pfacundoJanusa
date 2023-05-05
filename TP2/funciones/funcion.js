f598417c9e430edcab4f14cfe38f56be71fc8a00
Janusa <129460842+janusa13@users.noreply.github.com>
2023-05-04 21:28:03 -0300

    Add files via upload

---
+++ TP2/funciones/funcion.js
@@ -0,0 +1,58 @@
+function consultar() {
+  //SE OBTIENE EL VALOR DEL ID//
+  let aux = document.getElementById("consulta").value;
+
+  //SE OBTIENE EL VALOR DE LA API Y SE FORMA LA URL CON EL ID//
+  fetch("https://rickandmortyapi.com/api/character/" + aux)
+
+    //SE TRANSFORMA LA INFORMACION A JSON//
+    .then(function (response) {
+      return response.json(response);
+    })
+
+    //SE CREAN ELEMENTOS PARA MOSTRAR LA INFORMACION DE LA API
+    .then(function (data) {
+      let numero = document.createElement("p");
+        numero.textContent = "Numero: " + data.id;
+      let nombre = document.createElement("p");
+        nombre.textContent = "Nombre: " + data.name;
+      let especie = document.createElement("p");
+        especie.textContent = "Especie: " + data.species;
+      let estado = document.createElement("p");
+        estado.textContent = "Estado: " + data.status;
+      let genero = document.createElement("p");
+        genero.textContent = "Genero: " + data.gender;
+      let origen = document.createElement("p");
+        origen.textContent = "Origen: " + data.location.name;
+      let imagen = document.createElement("img");
+          imagen.src=data.image;
+          alert(imagen);
+      let episodios = document.createElement("p");
+          episodios.textContent= "Cantidad de Episodios: "+data.episode.length;
+
+
+
+    
+      //VERIFICACION DE LOS DATOS
+      console.log(aux);
+      console.log(imagen);
+
+      //LOS ELEMENTOS CREADOS ANTERIORMENTE SE DECLARAN HIJOS DE RESULTADOS
+      //PARA MOSTRARLOS EN EL HTML
+      let resultado = document.getElementById("resultado");
+
+      //vaciar el resultado
+      resultado.innerHTML = "";
+  
+
+      //SE CREAN HIJOS DE RESULTADOS CON LOS DATOS. 
+      resultado.appendChild(imagen);
+      resultado.appendChild(numero);
+      resultado.appendChild(nombre);
+      resultado.appendChild(especie);
+      resultado.appendChild(estado);
+      resultado.appendChild(genero);
+      resultado.appendChild(origen);
+      resultado.appendChild(episodios);
+    });
+}
